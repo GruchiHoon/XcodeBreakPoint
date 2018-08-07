cb20bf47c93c7df0d6197abb6e0b1dc0425ea6e7
이성훈 <LeeSungHun@iseonghun-ui-macbook-pro.local>
2018-08-07 15:08:47 +0900

    sum - plus

--- XcodeBreakPoint/ViewController.swift
+++ XcodeBreakPoint/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var sum = 0
+    var sum2 = 3
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var count = 0
+        for row in 5...10{
+            count += 1
+            self.sum += row
+        }
+        
+        print("총합은 \(self.sum),\(count)회 실행되엇습니다")
     }
 
     override func didReceiveMemoryWarning() {
